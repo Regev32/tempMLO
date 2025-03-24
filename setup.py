cd8d8326d555ac234b43e40b4bd98ca3419285d2
yehezkr7 <regevel2006@gmail.com>
2025-03-24 16:23:03 +0200

    Trying to make this repo pip installable

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='tempMLO',
+    version='0.1',
+    py_modules=['RunGrim', 'filter_top_3', 'filter_by_rest'],  # list each module file
+)
